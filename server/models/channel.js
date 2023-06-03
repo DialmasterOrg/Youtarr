4cf1f01885287bbeaf125816131b38cff4a86169
Chris Dial <dial.chris@gmail.com>
2023-06-03 07:14:32 -0700

    feat(#5): Fetch and store channel info for display
    * Fetch channel info using yt-dlp. Store in in a channels table.
      Display channel 'uploader' where available.
      Also some work to store poster images per video for future work

---
+++ server/models/channel.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const { sequelize } = require('../db');
+
+class Channel extends Model {}
+
+Channel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    channel_id: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    uploader: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Channel',
+    timestamps: false,
+    tableName: 'channels',
+  }
+);
+
+module.exports = Channel;
