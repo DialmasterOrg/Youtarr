0c50ef9b8bbd2b63c8eee2fed05e9bd13304e1b4
Chris Dial <dial.chris@gmail.com>
2023-06-13 09:59:25 -0700

    feat(#54): Remove unneccesary comment

--- migrations/20230613070141-Add_Last_Fetched_To_Channels_Table.js
+++ migrations/20230613070141-Add_Last_Fetched_To_Channels_Table.js
@@ -5,7 +5,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('channels', 'lastFetched', {
       type: Sequelize.DATE,
-      allowNull: true, // Change to 'false' if the field should be required
+      allowNull: true,
     });
   },
 
