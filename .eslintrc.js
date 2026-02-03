4f630d2297d9b1a1093cb6452885c4fe052b808f
bballdavis <bballdavis@hotmail.com>
2026-02-03 10:49:32 -0600

    Revert to align with upstream eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+﻿module.exports = {
   settings: {
     react: {
       version: "18.2",
@@ -7,15 +7,8 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:testing-library/react",
-  ],
+  extends: "plugin:react/recommended",
   overrides: [
     {
       files: ["server/**/__tests__/**/*.js", "server/**/*.test.js"],
@@ -28,23 +21,9 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
-  plugins: ["react", "@typescript-eslint", "react-hooks", "testing-library"],
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
   rules: {
-    "react/no-unescaped-entities": "off",
     "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "no-case-declarations": "off",
-    "prefer-const": "off",
-    "react-hooks/globals": "off",
-    "react-hooks/immutability": "off",
-    "react-hooks/set-state-in-effect": "off",
-    "react-hooks/immutable-state": "off",
   },
 };
