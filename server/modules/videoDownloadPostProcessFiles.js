2eccc4b997d8531522f1ff13561a0a43b0aaa820
Jonah Updegrove <jonah.updegrove@icloud.com>
2026-08-23 17:38:47 -0400

    Fix channelRecord.additional_tags being passed as an argument when null (npm test was unhappy before)

--- server/modules/videoDownloadPostProcessFiles.js
+++ server/modules/videoDownloadPostProcessFiles.js
@@ -456,7 +456,7 @@ async function resolveTrackedOwnerChannelId(youtubeId, metadataChannelId) {
 
     // Generate NFO file for Jellyfin/Kodi/Emby compatibility if enabled
     if (shouldWriteVideoNfoFiles()) {
-      nfoGenerator.writeVideoNfoFile(videoPath, jsonData, channelRecord.additional_tags);
+      nfoGenerator.writeVideoNfoFile(videoPath, jsonData, ...(channelRecord ? [channelRecord.additional_tags] : []));
     }
 
     // Check if this is an audio file (MP3) - skip video-specific metadata embedding
