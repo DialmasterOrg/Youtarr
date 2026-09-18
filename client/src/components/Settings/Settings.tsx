1e27b034f2baae0dc37baf9791a9ee029c09e815
Philip Davis <46248315+bballdavis@users.noreply.github.com>
2026-09-17 22:28:19 -0500

    feat: pass external API capability to key settings

--- client/src/components/Settings/Settings.tsx
+++ client/src/components/Settings/Settings.tsx
@@ -474,6 +474,7 @@ export function Settings({ token, onRequestsNavLinkPreview }: SettingsProps) {
                 token={token}
                 apiKeyRateLimit={config.apiKeyRateLimit}
                 onRateLimitChange={(value) => handleConfigChange({ apiKeyRateLimit: value })}
+                externalApiEnabled={isPlatformManaged.externalApiEnabled}
                 showRequestsNavLink={config.showRequestsNavLink}
                 onShowRequestsNavLinkChange={(value) => {
                   handleConfigChange({ showRequestsNavLink: value });
